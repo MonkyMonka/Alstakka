970a9ee744de0cd82bc00756a45c8985e072586b
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-02-28 23:32:34 -0500

    Token Item initial recipe

--- src/main/java/com/monky/alstakka/datagen/ModRecipeProvider.java
+++ src/main/java/com/monky/alstakka/datagen/ModRecipeProvider.java
@@ -1,10 +1,12 @@
 package com.monky.alstakka.datagen;
 
 import com.monky.alstakka.block.ModBlocks;
+import com.monky.alstakka.item.ModItems;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.function.Consumer;
@@ -19,11 +21,19 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BOTTLE_RACK_BLOCK.get())
                 .define('B', ItemTags.WOODEN_SLABS)
-                .pattern("BBB")
-                .pattern("BBB")
-                .pattern("BBB")
+                .pattern("WWW")
+                .pattern("WWW")
+                .pattern("WWW")
                 .unlockedBy("has_" + ItemTags.WOODEN_SLABS.location(), inventoryTrigger(ItemPredicate.Builder.item()
                         .of(ItemTags.WOODEN_SLABS).build()))
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.TOKEN_ITEM.get())
+                .define('C', Items.CHAIN.asItem())
+                .pattern("C")
+                .pattern("C")
+                .unlockedBy("has_" + Items.CHAIN.getDescriptionId(), inventoryTrigger(ItemPredicate.Builder.item()
+                        .of(Items.CHAIN.asItem()).build()))
                 .save(consumer);
     }
 }
