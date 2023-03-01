eb574dba34a62fadf95b318f7b04d599b3c9c0e1
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-03-01 00:15:47 -0500

    Mixins Setup!

---
+++ src/main/java/com/monky/alstakka/mixin/MixinDeathScreen.java
@@ -0,0 +1,8 @@
+package com.monky.alstakka.mixin;
+
+import net.minecraft.client.gui.screens.DeathScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DeathScreen.class)
+public class MixinDeathScreen {
+}
