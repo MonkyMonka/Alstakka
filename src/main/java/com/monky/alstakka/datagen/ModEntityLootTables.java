7797fd333bbb2bfd4779af0765a59b61d619bc77
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-03-02 00:39:49 -0500

    dip

--- src/main/java/com/monky/alstakka/datagen/ModEntityLootTables.java
+++ src/main/java/com/monky/alstakka/datagen/ModEntityLootTables.java
@@ -6,8 +6,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraftforge.registries.RegistryObject;
 
-import javax.swing.text.html.parser.Entity;
-
 
 public class ModEntityLootTables extends EntityLootSubProvider {
     public ModEntityLootTables() {
@@ -21,7 +19,7 @@ public class ModEntityLootTables extends EntityLootSubProvider {
     }
 
     @Override
-    protected Iterable<EntityType<?>> getKnownEntities() {
+    protected Iterable<EntityType>  getKnownEntityTypes() {
         return ModEntityTypes.ENTITY_TYPES.getEntries().stream().map(RegistryObject::get)::iterator;
     }
 
--- src/main/java/com/monky/alstakka/datagen/ModLootTableProvider.java
+++ src/main/java/com/monky/alstakka/datagen/ModLootTableProvider.java
@@ -10,6 +10,8 @@ import java.util.Set;
 public class ModLootTableProvider {
     public static LootTableProvider create(PackOutput output) {
         return new LootTableProvider(output, Set.of(),
-                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)));
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK),
+                        new LootTableProvider.SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)));
+
     }
 }
